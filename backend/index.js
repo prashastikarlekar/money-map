ab9c1d79cabda40d1137e5f2b912e366155c41a3
Prashasti Karlekar <prashastikarlekar@gmail.com>
2024-08-01 14:54:09 -0500

    Project setup, user type defined

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+const { url } = await startStandaloneServer(server);
+
+console.log(`🚀 Server ready at ${url}`);
